7e1ca6971f5aa97e11f2b04c35dde387fced6f4e
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-11-09 19:12:47 +0200

    reset input after submit the form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -14,7 +14,9 @@ const AddUser = props => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        console.log(enteredUsername,enteredAge)
+        console.log(enteredUsername, enteredAge);
+        setEnteredAge('');
+        setEnteredUsername('');
     };
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
@@ -27,9 +29,19 @@ const AddUser = props => {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" onChange={usernameChangeHandler}/>
+                <input
+                    id="username"
+                    type="text"
+                    value={enteredUsername}
+                    onChange={usernameChangeHandler}
+                />
                 <label htmlFor="age">Age (Year)</label>
-                <input id="age" type="number" onChange={ageChangeHandler}/>
+                <input
+                    id="age"
+                    type="number"
+                    value={enteredAge}
+                    onChange={ageChangeHandler}
+                />
                 <Button type="submit" onClick={addUserHandler}>Add User</Button>
             </form>
         </Card>
