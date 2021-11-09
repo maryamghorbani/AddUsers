f547adef3ef075a6f47901b2ee8f0066c096ab0b
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-11-09 21:29:26 +0200

    add users to list

--- src/App.js
+++ src/App.js
@@ -7,11 +7,19 @@ import UsersList from "./components/Users/UsersList";
 
 function App() {
     const [usersList, setUsersList] = useState([]);
-    
+
+    const addUserHandler = (uName, uAge) => {
+        setUsersList((prevUsersList) => {
+            return [
+                ...prevUsersList,
+                {name: uName, age: uAge, id: Math.random().toString()}];
+        });
+    };
+
     return (
         <div>
-            <AddUser/>
-            <UsersList users={[]}/>
+            <AddUser onAddUser={addUserHandler}/>
+            <UsersList users={usersList}/>
         </div>
     );
 }
