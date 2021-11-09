c939ad3862e0f74082e93d47542ae6d9a20f15a2
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-11-09 18:21:39 +0200

    add styles to card dynamicly

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,13 +3,16 @@ import React from 'react';
 // import components
 import Card from "../UI/Card";
 
+//import styles
+import classes from "./AddUser.module.css"
+
 const AddUser = props => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
     }
     return (
-        <Card>
+        <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
                 <input id="username" type="text"/>
