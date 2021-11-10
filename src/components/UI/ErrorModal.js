621a48d5d2e530ae284406a6eb89ebb9f206d736
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-11-10 14:02:09 +0200

    create component for error modal

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+//import components
+import Card from "./Card";
+import Button from "./Button";
+
+const ErrorModal = props => {
+    return (
+        <Card>
+            <header>
+                <h2></h2>
+            </header>
+            <div>
+                <p></p>
+            </div>
+            <footer>
+                <Button>Okay!</Button>
+            </footer>
+        </Card>
+    )
+};
+
+export default ErrorModal;
